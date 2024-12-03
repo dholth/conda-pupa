832ce8a2c57973cff81db8272a412457a9525344
Daniel Holth <dholth@fastmail.fm>
2024-12-03 14:31:58 -0500

    do editable from cli

--- conda_pupa/cli.py
+++ conda_pupa/cli.py
@@ -4,6 +4,8 @@ Command line interface for conda-pupa.
 
 import click
 
+import conda_pupa.editable
+
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.option(
@@ -16,7 +18,7 @@ import click
     "-e",
     "--editable",
     required=False,
-    help="Install local directory as editable package.",
+    help="Build named path as editable package; install to link checkout to environment.",
 )
 @click.option(
     "-p",
@@ -27,3 +29,6 @@ import click
 @click.option("-n", "--name", help="Name of environment.", required=False)
 def cli(channel, editable, prefix, name):
     print(channel, editable, prefix, name)
+
+    if editable:
+        conda_pupa.editable.editable(editable)
